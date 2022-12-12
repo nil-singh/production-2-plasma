e686a76421a80d9ff1ebcd75e6d18a762497dd75
Nilesh Singh <nileshsingh330@gmail.com>
2022-12-12 13:22:25 +0530

    code added 2

---
+++ client/build/precache-manifest.42b73f040005f2e279757fa738bd460e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eaadbce0c18fd42ce7d5d538e826e26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7a048c089c02fc9f66",
+    "url": "/static/css/main.34dd323e.chunk.css"
+  },
+  {
+    "revision": "3c869da1907acf64f6fb",
+    "url": "/static/js/2.880da336.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.880da336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7a048c089c02fc9f66",
+    "url": "/static/js/main.79333db4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d0480e5d5d76989e72ad9f0b5730c662",
+    "url": "/static/media/div.jpg.d0480e5d.jpg"
+  },
+  {
+    "revision": "8e7005f3e1db3bee210958664827a08f",
+    "url": "/static/media/nil.8e7005f3.jpg"
+  },
+  {
+    "revision": "1caf795867113962fb5d9134bcddcd62",
+    "url": "/static/media/plasmaforsure.1caf7958.png"
+  },
+  {
+    "revision": "5452cce9ea0e132489672144c989d8d1",
+    "url": "/static/media/sam.jpg.5452cce9.jpg"
+  }
+]);
